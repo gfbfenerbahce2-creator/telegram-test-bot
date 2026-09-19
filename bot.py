f02a3591adada74d10a6be69afe0db899181003f
gfbfenerbahce2-creator <gfbfenerbahce2@gmail.com>
2026-09-19 14:57:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
-print("Telegram test otomasyonu başladı!")
+import os
+import time
+
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+
+if not TOKEN:
+    raise RuntimeError("TELEGRAM_BOT_TOKEN bulunamadı!")
+
+print("Bot sistemi başladı.")
+
+while True:
+    print("Sistem çalışıyor...")
+    time.sleep(60)
